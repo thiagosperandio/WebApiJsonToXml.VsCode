c8be2cb5b4f8d67a0c8494b0c85288f1e007c064
Thiago Sperandio <thiagosperandio@gmail.com>
2019-06-06 21:03:17 -0300

    Versão WebApi 1 - funcionando basicamente

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebServiceJsonToXmlApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            HttpClient request = new HttpClient();
+            var responseClient = await request.GetStringAsync("https://www.redesocialdecidades.org.br/cities");
+            XNode node = Newtonsoft.Json.JsonConvert.DeserializeXNode(responseClient, "Root");
+            return Ok(node.ToString());
+        }
+    }
+}
